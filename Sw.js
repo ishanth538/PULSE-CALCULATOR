8b4fff8103cf44f75363cbe809c477923f37ba82
ishanth538 <jhemanthkumar1415@gmail.com>
2026-09-12 12:36:58 +0530

    Add files via upload

---
+++ Sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "pulse-calculator-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./Style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
